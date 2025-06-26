25300514da7c15c9619e39e02194feedd5dda3ee
cfssapp <57085427+cfssapp@users.noreply.github.com>
2025-06-26 03:26:21 +0000

    save

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ import time
 from tempfile import NamedTemporaryFile
 
 # App title
-# st.set_page_config(page_title='🎈 AI App', page_icon='🎈')
-st.title("🎈 AI App")
+st.set_page_config(page_title='🎈 AI App', page_icon='🎈')
+st.title("AI App 🎈")
 st.info(
     "In this app you can **Show** (provide mock-up image) or **Tell** (provide text prompt) how you want your Streamlit app to be built."
 )
